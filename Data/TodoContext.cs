5aa9b4d33bce67e60c5ff804162781912a87f5a9
Brian Jablonsky <bjablonsky@users.noreply.github.com>
2020-11-07 10:17:13 -0500

    Initial commit

---
+++ Data/TodoContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using week_7_project.Models;
+
+namespace week_7_project.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ Models/TodoItem.cs
@@ -0,0 +1,9 @@
+namespace week_7_project.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
